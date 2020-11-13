4a223bf5774a02a952db50ffd2cf4ee5525c2cd8
Merve Ogretmek <merve.ogretmek>
2020-11-13 05:43:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# my_first_flutter_app
+
+Motivational App
+
+This app is for the workshop held by Developer Student Clubs Middle East Technical University.
+
+## Getting Started
+
+In this app, aim is to learn some basic UI elements.
+
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // MaterialApp class: An application that uses material design.
+    MaterialApp(
+      // home property: This is the route that is displayed first when the application is started normally
+      // Scaffold class: Implements the basic material design visual layout structure.
+      home: Scaffold(
+          //backgroundColor property: Background color of the current widget
+          backgroundColor: Colors.blue[100],
+          //appBar property: An app bar to display at the top of the scaffold.
+          appBar: AppBar(
+            //appBar class: A material design app bar.
+            //title property: A title to display in app bar.
+            //Center class: A widget that centers its child within itself.
+            title: Center(
+              //child property: A widget that is in another widget.
+              child: Text('Motivational App'),
+            ),
+            //backgroundColor property: Background color of the current widget
+            backgroundColor: Colors.blue[300],
+          ),
+          // body property: The primary content of the scaffold.
+          body: Center(
+            // Image class: A widget that displays an image. --> duy
+            child: Image(
+              // image: AssetImage --> ampul
+              // AssetImage class: Fetches an image from an AssetBundle, having determined the exact image to use based on the context.
+              image: AssetImage('images/motivational.png'),
+            ),
+          )),
+    ),
+  );
+}
